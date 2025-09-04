83011a0cea56df85b4da46dc0952cb3e34211859
konofies <konofies@SSCLSERVICE1>
2025-09-04 15:40:15 +0800

    Clean code part2

--- MULTI_BENE.sql
+++ MULTI_BENE.sql
@@ -113,8 +113,13 @@ BEGIN
 END
 
 BEGIN
-UPDATE AML_TRANSACTION SET cpaccountno = BENEFICIARY_ACCOUNTNO, BENEFICIARY_ACCOUNTNO = NULL
-WHERE  TXNTYPE  = 'COCKD' AND  BENEFICIARY_ACCOUNTNO is not NULL and CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE())  AND SOURCE = 'CASA'
+    UPDATE AML_TRANSACTION 
+    SET cpaccountno = BENEFICIARY_ACCOUNTNO,
+        BENEFICIARY_ACCOUNTNO = NULL
+    WHERE TXNTYPE = 'COCKD'
+        AND  BENEFICIARY_ACCOUNTNO is not NULL 
+        and CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE())  
+        AND SOURCE = 'CASA'
 END
 
 /**************************************************************************/
@@ -122,203 +127,206 @@ END
 ---------------------------------------------Code xx
 ----------------------------------------2019-06-29
 BEGIN
-update AML_TRANSACTION set  
- BENEFICIARY_CUSTOMERREFNO = NULL
-,BENEFICIARY_DATEOFBIRTH  = NULL
-,BENEFICIARY_IDNo = NULL
-,BENEFICIARY_IDType = NULL
-,BENEFICIARY_NatureofBusiness = NULL
-,BENEFICIARY_PLACEOFBIRTH = NULL
-,BENEFICIARYADDRESS1 = NULL
-,BENEFICIARYADDRESS2 = NULL
-,BENEFICIARYADDRESS3 = NULL
-,BENEFICIARYNAME1 = NULL
-,BENEFICIARYNAME2 = NULL
-,BENEFICIARYNAME3 = NULL
-,BENEFICIARY_NAME_FLAG = NULL
-,BENEFICIARY_ACCOUNTNO = NULL
-WHERE TXNTYPE NOT IN 
-(
-	SELECT DISTINCT TRAN_CODE FROM AML_REGULATORY_REPORT_MANDATORY BB
-	INNER JOIN AML_FIELD CC ON BB.FIELD_ID = CC.FIELD_ID
-	WHERE CC.DATASET_NAME = 'CTR1_Beneficiary'
-		AND (FIELD_NAME = 'B_LASTNAME' OR FIELD_NAME = 'B_ACCOUNTNO' OR FIELD_NAME = 'B_FIRSTNAME')
-) 
-AND TXNTYPE in (select distinct TRANSACTION_TYPE from AML_CTR_TRANTYPE)
-and (BENEFICIARYNAME1 is not null or BENEFICIARYNAME3 is not null or BENEFICIARY_ACCOUNTNO is not null)
-AND CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE())  AND SOURCE = 'CASA'
+    update AML_TRANSACTION 
+    set BENEFICIARY_CUSTOMERREFNO = NULL,
+        BENEFICIARY_DATEOFBIRTH  = NULL,
+        BENEFICIARY_IDNo = NULL,
+        BENEFICIARY_IDType = NULL,
+        BENEFICIARY_NatureofBusiness = NULL,
+        BENEFICIARY_PLACEOFBIRTH = NULL,
+        BENEFICIARYADDRESS1 = NULL,
+        BENEFICIARYADDRESS2 = NULL,
+        BENEFICIARYADDRESS3 = NULL,
+        BENEFICIARYNAME1 = NULL,
+        BENEFICIARYNAME2 = NULL,
+        BENEFICIARYNAME3 = NULL,
+        BENEFICIARY_NAME_FLAG = NULL,
+        BENEFICIARY_ACCOUNTNO = NULL
+    WHERE TXNTYPE NOT IN 
+        (
+	        SELECT DISTINCT TRAN_CODE 
+            FROM AML_REGULATORY_REPORT_MANDATORY BB
+	            JOIN AML_FIELD CC ON BB.FIELD_ID = CC.FIELD_ID
+	        WHERE CC.DATASET_NAME = 'CTR1_Beneficiary'
+		        AND (
+                    FIELD_NAME = 'B_LASTNAME' 
+                    OR FIELD_NAME = 'B_ACCOUNTNO' 
+                    OR FIELD_NAME = 'B_FIRSTNAME'
+                   )
+        ) 
+        AND TXNTYPE in (
+            select distinct TRANSACTION_TYPE 
+            from AML_CTR_TRANTYPE
+        )
+        and (BENEFICIARYNAME1 is not null or BENEFICIARYNAME3 is not null or BENEFICIARY_ACCOUNTNO is not null)
+        AND CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE())  AND SOURCE = 'CASA'
 END
 
 
 BEGIN
 /***********SET THE DEFAULT BENEACCOUNTNUMBER FROM Transaction From/To Bank GL Account TO 240180104 ******************/
-UPDATE AML_TRANSACTION SET BENEFICIARY_ACCOUNTNO = '240180104'
-WHERE CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE()) AND TXNTYPE = 'CTRIA'
-AND LTRIM(RTRIM(BENEFICIARYNAME1)) = 'Transaction From/To Bank GL Account'  AND SOURCE = 'CASA'
+
+    UPDATE AML_TRANSACTION 
+    SET BENEFICIARY_ACCOUNTNO = '240180104'
+    WHERE CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE()) 
+        AND TXNTYPE = 'CTRIA'
+        AND LTRIM(RTRIM(BENEFICIARYNAME1)) = 'Transaction From/To Bank GL Account'  
+        AND SOURCE = 'CASA'
+
 /***********SET THE DEFAULT BENEACCOUNTNUMBER FROM Transaction From/To Bank GL Account TO 240180104 ******************/
 END
 
 Begin
 
-UPDATE AML_TRANSACTION SET TXN_DATE_TIME=  TXNDATE   WHERE  CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE()) 
-UPDATE AML_TRANSACTION SET txndate = CONVERT(DATE,TXNDATE) WHERE  CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE()) 
+    UPDATE AML_TRANSACTION 
+    SET TXN_DATE_TIME=  TXNDATE   
+    WHERE CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE()) 
+
+    UPDATE AML_TRANSACTION 
+    SET txndate = CONVERT(DATE,TXNDATE) 
+    WHERE CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE()) 
 END
 
 begin
 
-UPDATE A SET TXNTYPE = 
-CASE WHEN ACCOUNTTYPE IN ('901','902','903','904','906','907','908','909','911','912','913','914') THEN 'DTDYK'
- ELSE'CTRIA'END
-from aml_transaction a
-left join aml_account b on a.accountnumber = b.ACCOUNTNUMBER
-where PRODUCTTYPE = 'FTRQ' AND TXNMODE = 'DR'
- and convert(date,A.IMPORTED_DATE) = CONVERT(DATE,GETDATE())
+    UPDATE A 
+    SET TXNTYPE = ( CASE WHEN ACCOUNTTYPE IN ('901','902','903','904','906','907','908','909','911','912','913','914') THEN 'DTDYK' ELSE'CTRIA'END )
+    from aml_transaction a
+        left join aml_account b on a.accountnumber = b.ACCOUNTNUMBER
+    where PRODUCTTYPE = 'FTRQ' 
+        AND TXNMODE = 'DR'
+        and convert(date,A.IMPORTED_DATE) = CONVERT(DATE,GETDATE())
 
 end
 
 
 BEGIN
-update a set txntype = 'DTDPM' FROM AML_TRANSACTION a left join ref_treasury_accounts b on a.ACCOUNTNUMBER =b.ACCOUNTNO
-WHERE --ACCOUNTNUMBER = '111120240242'
-b.accountno is not null and producttype = 'ftrq' 
-AND CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE())
+    update a 
+    set txntype = 'DTDPM' 
+    FROM AML_TRANSACTION a 
+        left join ref_treasury_accounts b on a.ACCOUNTNUMBER =b.ACCOUNTNO
+    WHERE --ACCOUNTNUMBER = '111120240242'
+        b.accountno is not null and producttype = 'ftrq' 
+    AND CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE())
+
 END
 
 
 
 BEGIN
-update a set txntype = 'DTDPD' FROM AML_TRANSACTION a left join ref_treasury_accounts b on a.ACCOUNTNUMBER =b.ACCOUNTNO
-WHERE --ACCOUNTNUMBER = '111120240242'
-b.accountno is not null and producttype = 'DEBK' 
-AND CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE())
-END
-
-begin
-delete a from aml_transaction a
-left join aml_account b on a.AccountNumber = b.ACCOUNTNUMBER
-where a.txntype IN ('DTDPD','DTDYK' )
-and ACCOUNTTYPE NOT IN ('901','902','903','904','906','907','908','909','911','912','913','914')
-end
-
-BEGIN
-UPDATE AML_TRANSACTION SET TXNTYPE = 'DTDYM' WHERE TXNTYPE = 'DTDPM'
+    update a 
+    set txntype = 'DTDPD' 
+    FROM AML_TRANSACTION a 
+        left join ref_treasury_accounts b on a.ACCOUNTNUMBER =b.ACCOUNTNO
+    WHERE --ACCOUNTNUMBER = '111120240242'
+        b.accountno is not null and producttype = 'DEBK' 
+        AND CONVERT(DATE,IMPORTED_DATE) = CONVERT(DATE,GETDATE())
 END
 
 begin
+    delete a 
+    from aml_transaction a
+        left join aml_account b on a.AccountNumber = b.ACCOUNTNUMBER
+    where a.txntype IN ('DTDPD','DTDYK' )
+        and ACCOUNTTYPE NOT IN ('901','902','903','904','906','907','908','909','911','912','913','914')
+end
 
-insert into AML_CUSTOMER_ACCOUNT ( CUSTOMERID, ACCOUNTID, JOINTID, IMPORTED_DATE)
-select a.CUSTOMERID, a.ACCOUNTID ,
- (select max(jointid) from AML_CUSTOMER_ACCOUNT) + ROW_NUMBER() over (order by a.accountid) as JointID
- ,getdate()
- from aml_account a
-left join Aml_customer_account ca
-on a.ACCOUNTID = ca.ACCOUNTID and a.CUSTOMERID = ca.CUSTOMERID
-where ca.JOINTID is null
+BEGIN
+    UPDATE AML_TRANSACTION 
+    SET TXNTYPE = 'DTDYM'
+    WHERE TXNTYPE = 'DTDPM'
+END
+
+begin
+    insert into AML_CUSTOMER_ACCOUNT ( CUSTOMERID, ACCOUNTID, JOINTID, IMPORTED_DATE)
+    select a.CUSTOMERID, 
+        a.ACCOUNTID ,
+        (select max(jointid) from AML_CUSTOMER_ACCOUNT) + ROW_NUMBER() over (order by a.accountid) as JointID,
+        getdate()
+    from aml_account a
+        left join Aml_customer_account ca on a.ACCOUNTID = ca.ACCOUNTID and a.CUSTOMERID = ca.CUSTOMERID
+    where ca.JOINTID is null
 end
 
 
 BEGIN
-UPDATE  A SET TXNTYPE = 'DTDYW'FROM AML_TRANSACTION A
-LEFT JOIN AML_ACCOUNT B ON A.AccountNumber = B.ACCOUNTNUMBER
-WHERE PRODUCTTYPE = 'FTRQ'
-AND B.OFF_EMP_ID = 4
+    UPDATE  A 
+    SET TXNTYPE = 'DTDYW'
+    FROM AML_TRANSACTION A
+        LEFT JOIN AML_ACCOUNT B ON A.AccountNumber = B.ACCOUNTNUMBER
+    WHERE PRODUCTTYPE = 'FTRQ'
+        AND B.OFF_EMP_ID = 4
 END
 
 BEGIN
 /*********** HAS BENEFICIARY ACCOUNT FOR CPMD/CTRIA ******************/
-UPDATE 
-  ATM 
-SET 
-  BENEFICIARY_CUSTOMERREFNO = '', 
-  BENEFICIARY_DATEOFBIRTH = '', 
-  BENEFICIARY_IDNo = '', 
-  BENEFICIARY_IDType = '', 
-  BENEFICIARY_NatureofBusiness = '', 
-  BENEFICIARY_PLACEOFBIRTH = '', 
-  BENEFICIARYADDRESS1 = '', 
-  BENEFICIARYADDRESS2 = '', 
-  BENEFICIARYADDRESS3 = '', 
-  BENEFICIARYNAME1 = CUST.FIRSTNAME, 
-  BENEFICIARYNAME2 = CUST.MIDDLENAME, 
-  BENEFICIARYNAME3 = CUST.LASTNAME, 
-  BENEFICIARY_NAME_FLAG = CASE WHEN CUST.INDIVIDUAL_CORPORATE = 1 THEN 'N' WHEN CUST.INDIVIDUAL_CORPORATE = 2 THEN 'Y' END
-FROM 
-  AML_TRANSACTION ATM 
-  INNER JOIN (
-    SELECT 
-      * 
-    FROM 
-      (
-        SELECT 
-          A.ACCOUNTID, 
-          B.TXNID, 
-          C.CUSTOMERID, 
-          ROW_NUMBER() OVER(
-            PARTITION BY A.ACCOUNTID, 
-            B.TXNID 
-            ORDER BY 
-              A.ACCOUNTID
-          ) COUNTS 
-        FROM 
-          AML_ACCOUNT A 
-          INNER JOIN (
-            select 
-              BENEFICIARY_ACCOUNTNO, 
-              TXNID 
-            from 
-              AML_TRANSACTION 
-            WHERE 
-              LTRIM(
-                RTRIM(BENEFICIARY_ACCOUNTNO))
-				IN (
+    UPDATE ATM 
+    SET BENEFICIARY_CUSTOMERREFNO = '', 
+        BENEFICIARY_DATEOFBIRTH = '', 
+        BENEFICIARY_IDNo = '', 
+        BENEFICIARY_IDType = '', 
+        BENEFICIARY_NatureofBusiness = '', 
+        BENEFICIARY_PLACEOFBIRTH = '', 
+        BENEFICIARYADDRESS1 = '', 
+        BENEFICIARYADDRESS2 = '', 
+        BENEFICIARYADDRESS3 = '', 
+        BENEFICIARYNAME1 = CUST.FIRSTNAME, 
+        BENEFICIARYNAME2 = CUST.MIDDLENAME, 
+        BENEFICIARYNAME3 = CUST.LASTNAME, 
+        BENEFICIARY_NAME_FLAG = CASE WHEN CUST.INDIVIDUAL_CORPORATE = 1 THEN 'N' WHEN CUST.INDIVIDUAL_CORPORATE = 2 THEN 'Y' END
+    FROM AML_TRANSACTION ATM 
+        JOIN (
+            SELECT * 
+            FROM 
+                (
                 SELECT 
-                  ACCOUNTNUMBER 
-                FROM 
-                  AML_ACCOUNT
-              ) 
-              AND SOURCE = 'CASA' 
-              AND TXNTYPE IN ('CPMD', 'CTRIA') 
-              AND CONVERT(DATE, IMPORTED_DATE) = CONVERT(
-                DATE, 
-                GETDATE()
-              )
-          ) B ON A.ACCOUNTNUMBER = LTRIM(
-            RTRIM(B.BENEFICIARY_ACCOUNTNO)
-          ) 
-          INNER JOIN AML_CUSTOMER_ACCOUNT C ON A.ACCOUNTID = C.ACCOUNTID 
-        GROUP BY 
-          A.ACCOUNTID, 
-          C.CUSTOMERID, 
-          B.TXNID
-      ) TBL1 
-    WHERE 
-      COUNTS = 1
-  ) ATM2 ON ATM.TXNID = ATM2.TXNID 
-  INNER JOIN AML_ACCOUNT ACCT ON ATM2.ACCOUNTID = ACCT.ACCOUNTID
-  INNER JOIN AML_CUSTOMER CUST ON CUST.CUSTOMERID = ATM2.CUSTOMERID
+                    A.ACCOUNTID, 
+                    B.TXNID, 
+                    C.CUSTOMERID, 
+                    ROW_NUMBER() OVER( PARTITION BY A.ACCOUNTID, B.TXNID  ORDER BY A.ACCOUNTID) COUNTS 
+                FROM AML_ACCOUNT A 
+                    JOIN (
+                        select BENEFICIARY_ACCOUNTNO, 
+                            TXNID 
+                        from AML_TRANSACTION 
+                        WHERE LTRIM( RTRIM(BENEFICIARY_ACCOUNTNO)) IN (
+                                SELECT ACCOUNTNUMBER FROM  AML_ACCOUNT
+                            ) 
+                            AND SOURCE = 'CASA' 
+                            AND TXNTYPE IN ('CPMD', 'CTRIA') 
+                            AND CONVERT(DATE, IMPORTED_DATE) = CONVERT(DATE, GETDATE() )
+                    ) B ON A.ACCOUNTNUMBER = LTRIM(RTRIM(B.BENEFICIARY_ACCOUNTNO)) 
+            JOIN AML_CUSTOMER_ACCOUNT C ON A.ACCOUNTID = C.ACCOUNTID 
+            GROUP BY 
+              A.ACCOUNTID, 
+              C.CUSTOMERID, 
+              B.TXNID
+          ) TBL1 
+        WHERE 
+          COUNTS = 1
+      ) ATM2 ON ATM.TXNID = ATM2.TXNID 
+      INNER JOIN AML_ACCOUNT ACCT ON ATM2.ACCOUNTID = ACCT.ACCOUNTID
+      INNER JOIN AML_CUSTOMER CUST ON CUST.CUSTOMERID = ATM2.CUSTOMERID
 /********************************************************************/
 END
 
 BEGIN
 /***************************** HAS NO BENEFICIARY ACCOUNT ***************************************/
-Update AML_TRANSACTION
-Set
-BENEFICIARY_NAME_FLAG = CASE WHEN BENEFICIARY_ACCOUNTNO IS NULL THEN 'Y'
-ELSE
-BENEFICIARY_NAME_FLAG
-END
-Where SOURCE = 'CASA' AND TXNTYPE IN ('CPMD', 'CTRIA', 'CCMC') 
-AND CONVERT(DATE, IMPORTED_DATE) = CONVERT(DATE, GETDATE())
+    Update AML_TRANSACTION       
+    Set BENEFICIARY_NAME_FLAG = CASE WHEN BENEFICIARY_ACCOUNTNO IS NULL THEN 'Y' ELSE BENEFICIARY_NAME_FLAG END
+    Where SOURCE = 'CASA' 
+        AND TXNTYPE IN ('CPMD', 'CTRIA', 'CCMC') 
+        AND CONVERT(DATE, IMPORTED_DATE) = CONVERT(DATE, GETDATE())
 END
 /********************************************************************/
 
 BEGIN
 /******************************** Remove BENEFICIARY_ACOUNTNO in CPMD ************************************/
-Update AML_TRANSACTION
-Set
-BENEFICIARY_ACCOUNTNO = NULL
-Where SOURCE = 'CASA' AND TXNTYPE = 'CPMD'
-AND CONVERT(DATE, IMPORTED_DATE) = CONVERT(DATE, GETDATE())
+    Update AML_TRANSACTION
+    Set BENEFICIARY_ACCOUNTNO = NULL
+    Where SOURCE = 'CASA' 
+    AND TXNTYPE = 'CPMD'
+    AND CONVERT(DATE, IMPORTED_DATE) = CONVERT(DATE, GETDATE())
 END
 
 END
